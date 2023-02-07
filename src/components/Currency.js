06321e83b6bef0daeb10887b87f2e3537775a041
annadavydova <lileynik@yahoo.com>
2023-02-07 11:56:50 -0500

    update

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,68 +1,69 @@
 function Currency() {
     return (<div>
 
-        <div className="d-inline-flex p-2">
-            <h2>
-                <button><strong>USD</strong></button>
-            </h2>' '
+            <div className="d-inline-flex p-2">
+                <h2 className="me-2">
+                    <button><strong>USD</strong></button>
+                </h2>
 
-            <h2>
-                <button><strong>EUR</strong></button>
-            </h2>' '
 
-            <h2>
-                <button><strong>GBP</strong></button>
-            </h2>' '
+                <h2 className="me-2">
+                    <button><strong>EUR</strong></button>
+                </h2>
 
-            <h2>
-                <button><strong>TRY</strong></button>
-            </h2>' '
-            <h2>
-                <button><strong>KZT</strong></button>
-                </h2>' '
-                    <h2>
-                        <button><strong>BYN</strong></button>
-                        </h2>' '
-                            <h2>
-                                <button><strong>PLN</strong></button>
-                            </h2>' '
-                                    <h2>
-                                        <button><strong>AED</strong></button>
-                                        </h2>' '
-                                            <h2>
-                                                <button><strong>SGD</strong></button>
-                                                </h2>' '
-                                                    <h2>
-                                                        <button><strong>CZK</strong></button>
-                                                        </h2>' '
-                                                            <h2>
-                                                                <button><strong>SEK</strong></button>
-                                                                </h2>' '
-                                                                    <h2>
-                                                                        <button><strong>NOK</strong></button>
-                                                                        </h2>' '
-                                                                            <h2>
-                                                                                <button><strong>CAD</strong>
-                                                                                </button>
-                                                                                </h2>' '
-                                                                                    <h2>
-                                                                                        <button><strong>CHF</strong>
-                                                                                        </button>
-                                                                                        </h2>' '
-                                                                                            <h2>
-                                                                                                <button>
-                                                                                                    <strong>DKK</strong>
-                                                                                                </button>
-                                                                                                </h2>' '
-                                                                                                    <h2>
-                                                                                                        <button>
-                                                                                                            <strong>JPY</strong>
-                                                                                                        </button>
-                                                                                                        </h2>
+                <h2 className="me-2">
+                    <button><strong>GBP</strong></button>
+                </h2>
+
+                <h2 className="me-2">
+                    <button><strong>TRY</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>KZT</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>BYN</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>PLN</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>AED</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>SGD</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>CZK</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>SEK</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>NOK</strong></button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>CAD</strong>
+                    </button>
+                </h2>
+                <h2 className="me-2">
+                    <button><strong>CHF</strong>
+                    </button>
+                </h2>
+                <h2 className="me-2">
+                    <button>
+                        <strong>DKK</strong>
+                    </button>
+                </h2>
+                <h2 className="me-2">
+                    <button>
+                        <strong>JPY</strong>
+                    </button>
+                </h2>
+            </div>
+
         </div>
-
-    </div>
-)
+    )
 }
 
 export default Currency;
